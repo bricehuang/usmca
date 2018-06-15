df76e00e4522a87d5ab1d01f273075f21eca05a2
Brice Huang <bmhuang@mit.edu>
2018-06-14 22:24:29 -0500

    Import cleanup

--- src/components/routes.js
+++ src/components/routes.js
@@ -3,35 +3,25 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import IndexPage from "./pages/index-page";
-import LoginPage from "./pages/login-page";
-import AccountPage from "./pages/account-page";
-import TestSolvePage from "./pages/test-solve-page";
-import ViewContestPage from "./pages/view-contest-page";
-import ViewTestPage from "./pages/view-test-page";
-import ViewDatabasePage from "./pages/view-database-page";
-import ViewProblemPage from "./pages/view-problem-page";
-import PublicDatabasePage from "./pages/public-database-page";
-import NotFoundPage from "./pages/not-found-page";
 import requireAuth from "./auth/require-auth";
 
 import * as Pages from "./pages";
 
 const Routes = ({ authenticated }) => (
   <Switch>
-    <Route exact path="/" component={ authenticated ? AccountPage : IndexPage } />
-    <Route path="/home/:tab" component={ authenticated ? AccountPage : IndexPage } />
-    <Route exact path="/login" component={ LoginPage }/>
+    <Route exact path="/" component={ authenticated ? Pages.AccountPage : Pages.IndexPage } />
+    <Route path="/home/:tab" component={ authenticated ? Pages.AccountPage : Pages.IndexPage } />
+    <Route exact path="/login" component={ Pages.LoginPage }/>
     <Route exact path="/propose" component={ requireAuth(Pages.Propose) }/>
-    <Route exact path="/test-solve" component={ requireAuth(TestSolvePage) }/>
-    <Route exact path="/public-database" component={ requireAuth(PublicDatabasePage) }/>
+    <Route exact path="/test-solve" component={ requireAuth(Pages.TestSolvePage) }/>
+    <Route exact path="/public-database" component={ requireAuth(Pages.PublicDatabasePage) }/>
     <Route exact path="/about" component={ Pages.About }/>;
-    <Route path="/view-contest/:id" component={ requireAuth(ViewContestPage) }/>
-    <Route path="/view-database/:id" component={ requireAuth(ViewDatabasePage) }/>
-    <Route path="/view-test/:id" component={ requireAuth(ViewTestPage) }/>
-    <Route path="/view-problem/:id" component={ requireAuth(ViewProblemPage) }/>
+    <Route path="/view-contest/:id" component={ requireAuth(Pages.ViewContestPage) }/>
+    <Route path="/view-database/:id" component={ requireAuth(Pages.ViewDatabasePage) }/>
+    <Route path="/view-test/:id" component={ requireAuth(Pages.ViewTestPage) }/>
+    <Route path="/view-problem/:id" component={ requireAuth(Pages.ViewProblemPage) }/>
     <Route path="/edit-problem/:id" component={ requireAuth(Pages.Propose) }/>
-    <Route path="*" component={ NotFoundPage } />
+    <Route path="*" component={ Pages.NotFoundPage } />
   </Switch>
 );
 
